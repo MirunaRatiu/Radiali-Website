d8a0277d1e0ce0cd1bc92be4ea9dd423d5ab9b4e
MirunaRatiu <mirunaratiu03@gmail.com>
2025-01-04 23:19:33 +0200

    animatie

--- productManagement/models.py
+++ productManagement/models.py
@@ -1,21 +1,12 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    image_url = models.URLField(max_length=2083)
-    slug = models.SlugField(unique=True)  # Slug for creating dynamic URLs
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.URLField(max_length=2083)
-
+    category =models.CharField(max_length=255)
     # category = models.ForeignKey(
     #     Category, related_name="products", on_delete=models.CASCADE
     # )
